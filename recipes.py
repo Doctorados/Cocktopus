5f3746e718345f341658803da831e12c8d79fddc
Doctorados <Doctorados@users.noreply.github.com>
2016-11-21 19:25:01 +0100

    Add files via upload

--- recipes.py
+++ recipes.py
@@ -16,7 +16,35 @@ class recipes:
         print("Cocktail is:", recipe)
         return recipe
 
-screwdriver = [0, 0, 4, 0, 0, 0, 0, 0, 10, 0]
+##################################################	Internet-Cocktails	##################################################
+screwdriver 		= [0, 0, 4, 0, 0, 0, 0, 0, 11, 0]
+thecockslaper		= [0, 4, 4, 0, 0, 0, 0, 2, 0, 4]
+schlumpf 			= [0, 0, 4, 0, 0, 2, 0, 0, 0, 4]
+orangefizz 			= [0, 5, 0, 0, 1, 0, 2, 0, 5, 0] #2cl Soda
+coolrussian 		= [0, 0, 5, 5, 2, 0, 3, 0, 0, 0] 
+wodkafizz			= [0, 0, 5, 0, 2, 0, 3, 0, 0, 0] #5cl Soda
+russischerspringer	= [0, 0, 6, 0, 1, 3, 5, 0, 0, 0] 
+bitterrussian 		= [1, 0, 4, 10, 0, 0, 0, 0, 0, 0]
+daiquiri			= [5, 0, 0, 0, 1, 0, 0.5, 0, 0, 0] #Tonne Eis
+Mai Tai				= [5, 0, 0, 0, 0, 2, 2, 3, 3, 0]
+pinapplefizz		= [5, 0, 0, 0, 1, 0, 3, 6, 0, 0]
+pinacolada			= [5, 0, 0, 0, 0, 0, 0, 5, 0, 0] #5cl Cocos
+tomcollins			= [0, 5, 0, 0, 2, 0, 3, 0, 0, 0] #5cl Soda
+##################################################	 Eigen-Cocktails	##################################################
+leberzirrhose 		= [3, 3, 3, 6, 0, 0, 0, 0, 0, 0]
+spassimglas 		= [0, 0, 4, 10, 0, 0, 0, 0, 0, 1]
+hotlinemiami 		= [3, 2, 0, 0, 0, 0, 5, 5, 0, 0]
+swimmingpool 		= [2, 0, 2, 0, 0, 1, 0, 6, 0, 2] #2cl Cocos
+manapotion			= [2, 2, 2, 0, 0, 2, 3, 0, 0, 4]
+##################################################	Alkfreie-Cocktails	##################################################
+springparadise		= [0, 0, 0, 0, 2, 0, 2, 3, 8, 0]
+sweetspring			= [0, 0, 0, 0, 1, 1, 1, 9, 3, 0]
+stechmuecke			= [0, 0, 0, 10, 1, 0, 3, 1, 0, 0]
 
-cocktails = [screwdriver, orangefizz, pinapplefizz, bitterrussian, pinacolada, ipanema, maitai, tomcollins, wodkafizz, springparadise, coolrussian, daiquiri, russischerspringer]
+
+
+
+
+
+cocktails = [screwdriver, orangefizz, pinapplefizz, bitterrussian, pinacolada, maitai, tomcollins, wodkafizz, springparadise, coolrussian, daiquiri, russischerspringer, sweetspring, stechmuecke]
 
